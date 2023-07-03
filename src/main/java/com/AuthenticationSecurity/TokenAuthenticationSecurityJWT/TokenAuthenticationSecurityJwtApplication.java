ccb44782b461bf59da0b95b661ec1687c7ae9e08
Matome <seletematome3@gmail.com>
2023-07-03 12:40:17 +0200

    first commit

---
+++ src/main/java/com/AuthenticationSecurity/TokenAuthenticationSecurityJWT/TokenAuthenticationSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.AuthenticationSecurity.TokenAuthenticationSecurityJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenAuthenticationSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenAuthenticationSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AuthenticationSecurity/TokenAuthenticationSecurityJWT/TokenAuthenticationSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AuthenticationSecurity.TokenAuthenticationSecurityJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenAuthenticationSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
