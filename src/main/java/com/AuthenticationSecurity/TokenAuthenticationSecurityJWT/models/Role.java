bf5e3dc82aa6d8c284b5006e70d068257b6b5225
Matome <seletematome3@gmail.com>
2023-07-04 10:59:27 +0200

    models working but jakarta not working yet

--- src/main/java/com/AuthenticationSecurity/TokenAuthenticationSecurityJWT/models/Role.java
+++ src/main/java/com/AuthenticationSecurity/TokenAuthenticationSecurityJWT/models/Role.java
@@ -1,4 +1,28 @@
 package com.AuthenticationSecurity.TokenAuthenticationSecurityJWT.models;
 
+//import jakarta.persistence.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    // getters and setters
 }
