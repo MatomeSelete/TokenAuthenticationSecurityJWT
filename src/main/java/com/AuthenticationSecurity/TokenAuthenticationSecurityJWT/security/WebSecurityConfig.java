f202d3fdd78bb90fd0dca260877d6e5c4c2da4dd
Matome <seletematome3@gmail.com>
2023-07-06 15:53:21 +0200

    still fixing the errors

--- src/main/java/com/AuthenticationSecurity/TokenAuthenticationSecurityJWT/security/WebSecurityConfig.java
+++ src/main/java/com/AuthenticationSecurity/TokenAuthenticationSecurityJWT/security/WebSecurityConfig.java
@@ -72,6 +72,7 @@ public class WebSecurityConfig {  // extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+
     }
 
 }
